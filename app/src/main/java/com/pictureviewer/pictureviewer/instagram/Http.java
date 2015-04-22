61c7f0bb8009b9d6da79f6948caf317010003b2f
mario_kusen <mkusen007@gmail.com>
2015-04-22 23:26:33 +0200

    v.0.0.3

--- app/src/main/java/com/pictureviewer/pictureviewer/instagram/Http.java
+++ app/src/main/java/com/pictureviewer/pictureviewer/instagram/Http.java
@@ -1,9 +1,13 @@
 package com.pictureviewer.pictureviewer.instagram;
 
+import android.util.Log;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -11,7 +15,10 @@ import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -19,60 +26,31 @@ import java.util.List;
  * Created by Mario on 19.4.2015..
  */
 public class Http {
-    static String response = null;
-    public final static int GET = 1;
-    public final static int POST = 2;
 
-    public Http() {
-
-    }
-
-    public String makeServiceCall(String url, int method) {
-        return this.makeServiceCall(url, method, null);
-    }
-
-    public String makeServiceCall(String url, int method,
-                                  List<NameValuePair> params) {
+    public static String readInstagram() {
+        StringBuilder builder = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("https://api.instagram.com/v1/media/popular?client_id=a957c5b858784410a87de3a90c95484b");
         try {
-            // http client
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpEntity httpEntity = null;
-            HttpResponse httpResponse = null;
-
-            // Checking http request method type
-            if (method == POST) {
-                HttpPost httpPost = new HttpPost(url);
-                // adding post params
-                if (params != null) {
-                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = client.execute(httpGet);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) {
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
                 }
-
-                httpResponse = httpClient.execute(httpPost);
-
-            } else if (method == GET) {
-                // appending params to url
-                if (params != null) {
-                    String paramString = URLEncodedUtils
-                            .format(params, "utf-8");
-                    url += "?" + paramString;
-                }
-                HttpGet httpGet = new HttpGet(url);
-
-                httpResponse = httpClient.execute(httpGet);
-
+            } else {
+                Log.e(ViewerInstagram.class.toString(), "Failed to download file");
             }
-            httpEntity = httpResponse.getEntity();
-            response = EntityUtils.toString(httpEntity);
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (ClientProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return response;
-
+        return builder.toString();
     }
 }
--- app/src/main/java/com/pictureviewer/pictureviewer/instagram/ViewerInstagram.java
+++ app/src/main/java/com/pictureviewer/pictureviewer/instagram/ViewerInstagram.java
@@ -1,31 +1,31 @@
 package com.pictureviewer.pictureviewer.instagram;
 
 import android.app.ListActivity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.pictureviewer.pictureviewer.R;
 import com.pictureviewer.pictureviewer.flickr.ViewerFlickr;
 
-import org.json.JSONArray;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by Mario on 18.4.2015..
@@ -35,141 +35,30 @@ public class ViewerInstagram extends ListActivity {
     //*TODO
     // PODESITI KOD DA HVATA I PARSIRA MOJ JSON*/
 
-
-    private ProgressDialog pDialog;
-
-    // URL to get data from JSON
-    private static String url = "https://api.instagram.com/v1/media/popular?client_id=a957c5b858784410a87de3a90c95484b";
-
-    // JSON Node names
-    private static final String DATA = "data";
-    private static final String PROFILE_PICTURE = "profile_picture";
-    private static final String TEXT = "text";
-    private static final String FULL_NAME = "full_name";
-
-    // data JSONArray
-    JSONArray data = null;
-
-    // Hashmap for ListView
-    ArrayList<HashMap<String, String>> dataList;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instagram);
-
-        dataList = new ArrayList<HashMap<String, String>>();
-
-        ListView listView = getListView();
-
-        // Listview on item click listener
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // getting values from selected ListItem
-                String naslov = ((TextView) view.findViewById(R.id.txtText))
-                        .getText().toString();
-                String autor = ((TextView) view.findViewById(R.id.txtAutor))
-                        .getText().toString();
-
-                // Starting single contact activity
-                Intent i = new Intent(getApplicationContext(),
-                        RedOpsirnije.class);
-                i.putExtra(TEXT, naslov);
-                i.putExtra(FULL_NAME, autor);
-                startActivity(i);
-
-            }
-        });
-
-        // Calling async task to get json
-        new GetData().execute();
-    }
-
-    /**
-     * Async task class to get json by making HTTP call
-     */
-    private class GetData extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            // Showing progress dialog
-            pDialog = new ProgressDialog(ViewerInstagram.this);
-            pDialog.setMessage("Please wait...");
-            pDialog.setCancelable(false);
-            pDialog.show();
-
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            // Creating service handler class instance
-            Http http = new Http();
-
-            // Making a request to url and getting response
-            String jsonStr = http.makeServiceCall(url, Http.GET);
-
-            Log.d("Response: ", "> " + jsonStr);
-
-            if (jsonStr != null) {
-                try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-
-                    // Getting JSON Array node
-                    data = jsonObj.getJSONArray(DATA);
-
-                    // looping through All Contacts
-                    for (int i = 0; i < data.length(); i++) {
-                        JSONObject jsonObject = data.getJSONObject(i);
-
-                        //  String slika = jsonObject.getString(PROFILE_PICTURE);
-                        String text = jsonObject.getString(TEXT);
-                        String autor = jsonObject.getString(FULL_NAME);
-
-
-                        // tmp hashmap for single contact
-                        HashMap<String, String> user = new HashMap<String, String>();
-
-                        // adding each child node to HashMap key => value
-                        // user.put(PROFILE_PICTURE, slika);
-                        user.put(TEXT, text);
-                        user.put(FULL_NAME, autor);
-
-                        // adding contact to contact list
-                        dataList.add(user);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Log.e("ServiceHandler", "Couldn't get any data from the url");
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            // Dismiss the progress dialog
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-            /**
-             * Updating parsed JSON data into ListView
-             * */
-            ListAdapter adapter = new SimpleAdapter(
-                    ViewerInstagram.this, dataList,
-                    R.layout.activity_instagram_viewer, new String[]{FULL_NAME,
-                    TEXT}, new int[]{
-                    R.id.txtAutor, R.id.txtText});
-
-            setListAdapter(adapter);
-        }
+        Reader();
 
     }
+
+    private void Reader() {
+        StrictMode.ThreadPolicy policy = new StrictMode.
+                ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String input = Http.readInstagram();
+
+        try {
+            JSONObject json = new JSONObject(input);
+            System.out.println("proba " + json);
+            Log.i(ViewerInstagram.class.getName(), json.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
